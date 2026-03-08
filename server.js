8e75bd61d60e423c2ec54bf41ff9c072c13b0bfd
Lucas Cunha <lucascunhagames@gmail.com>
2026-03-08 15:34:48 -0300

    Implement create and get order endpoints

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 
 const app = express();
 const PORT = 3000;
+const orders = [];
 
 app.use(express.json());
 
@@ -11,4 +12,35 @@ app.get("/", (req, res) => {
 
 app.listen(PORT, () => {
   console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
+
+app.post("/order", (req, res) => {
+  const order = req.body;
+
+  const mappedOrder = {
+    orderId: order.numeroPedido,
+    value: order.valorTotal,
+    creationDate: order.dataCriacao,
+    items: order.items.map(item => ({
+      productId: item.idItem,
+      quantity: item.quantidadeItem,
+      price: item.valorItem
+    }))
+  };
+
+  orders.push(mappedOrder);
+
+  res.status(201).json(mappedOrder);
+});
+
+app.get("/order/:orderId", (req, res) => {
+  const { orderId } = req.params;
+
+  const order = orders.find(order => order.orderId === orderId);
+
+  if (!order) {
+    return res.status(404).json({ error: "Pedido não encontrado" });
+  }
+
+  res.status(200).json(order);
 });
